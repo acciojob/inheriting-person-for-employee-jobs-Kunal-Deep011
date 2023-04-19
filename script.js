bf2340d16cbb97d9c02e56f99696b03ca4d48aa3
Kunal-Deep011 <117732649+Kunal-Deep011@users.noreply.github.com>
2023-04-20 00:51:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ function Person(name, age) {
 Person.prototype.greet = function() {
   console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 };
-function Employee(name, age, jobTitle) {
-  Person.call(this, name, age);
+function Employee(jobTitle) {
+ // Person.call(this, name, age);
   this.jobTitle = jobTitle;
 }
 
-Employee.prototype = Object.create(Person.prototype);
-Employee.prototype.constructor = Employee;
+Employee.prototype.__proto__ = Object.create(Person.prototype);
+//Employee.prototype.constructor = Employee;
 
 Employee.prototype.jobGreet = function() {
   console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
