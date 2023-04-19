5000595d186bc0ea22e61ff6f83fbcbd676f0fbe
Kunal-Deep011 <117732649+Kunal-Deep011@users.noreply.github.com>
2023-04-20 00:08:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,20 +2,20 @@
 function Person(name, age) {
        this._name = name;
 	   this._age = age;
-	this.greet = function(){
-		console.log("Hello, my name is "+this._name+," I am "+this._age+" years old.");
-	}
 }
-
+Person.prototype.greet = function(){
+	console.log("Hello, my name is "+this._name+," I am "+this._age+" years old.");
+}
 function Employee(name, age, jobTitle) {
+	this._name = name;
+	this._age = age;
 	this._jobTitle = jobTitle;
-	super(name, age)
-
-	this.jobGreet = function(){
+}
+Employee.prototype.jobGreet = function(){
 		console.log("Hello, my name is "+this._name+," I am"+this._age+" years old, and my job title is "+this._jobTitle+".);
 					
 	}
-}
+Person.prototype.__proto__ = Employee.prototype;
 
 // Do not change code below this line
 window.Person = Person;
